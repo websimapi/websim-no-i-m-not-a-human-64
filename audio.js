d8dd87b537fe0fbc8e798436d9d079887ca2b722
api <api@websim.ai>
2025-09-19 04:24:57 +0000

    v4: error: Uncaught SyntaxError: Unexpected end of input SyntaxError: Unexpected end of input

--- audio.js
+++ audio.js
@@ -1,5 +1,3 @@
-
-```javascript
 const AUDIO_DURATION = 95, FADE = 15, FADE_OUT_START = 80;
 export const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let audioUnlocked = false;
